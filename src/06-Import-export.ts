7487017f1f2c7397640a240767de77927c43a31e
Emma Ramos <20001039@alumnos.utleon.edu.mx>
2024-09-11 23:47:43 -0600

    segundo y tercer commit

---
+++ src/06-Import-export.ts
@@ -0,0 +1,20 @@
+import{calcularIVA2,producto} from"./05-desestructura-funciones";
+
+const carrito:producto[]=[   'carrito'
+     {
+        desc:'Telelefono1',
+        precio:100
+     },
+     {
+        desc:'Telelefono2',
+        precio:1000
+     },
+     {
+        desc:'Telelefono3',
+        precio:10000
+     }
+
+]
+const [total,iva]=calcularIVA2(carrito);
+console.log('total:  ${total}');
+console.log('total:  ${iva}');
